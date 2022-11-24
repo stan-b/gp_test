ffbe4a530154486c125c8fc234e6ce0db10b1af9
stan-b <blinov.stanislav@gmail.com>
2022-11-24 18:17:08 +0300

    added search query

--- parser.pl
+++ parser.pl
@@ -4,14 +4,13 @@ use strict;
 use warnings;
 use Data::Dumper;
 use DBI;
- 
+
 # Connect to the database.
-my $dbh = DBI->connect("DBI:mysql:database=gazprom;host=localhost",
-                       "stan", "aqua32",
-                       {'RaiseError' => 1});
+my $dbh = DBI->connect("DBI:mysql:database=gazprom;host=localhost", "stan", "aqua32", { 'RaiseError' => 1 });
 
 my $filename = 'out';
 my $counter = 0;
+
 # названия индексов для понятности
 
 my $message_date = 0;
@@ -23,37 +22,36 @@ my $message_other = 5;
 
 my $handle;
 unless (open $handle, "<:encoding(utf8)", $filename) {
-   print STDERR "Ошибка открытия файла '$filename': $!\n";
+  print STDERR "Ошибка открытия файла '$filename': $!\n";
 
-   return undef
+  return undef;
 }
 chomp(my @lines = <$handle>);
 unless (close $handle) {
-   print STDERR "Невозможно закрыть файл '$filename': $!\n";
-};
+  print STDERR "Невозможно закрыть файл '$filename': $!\n";
+}
 
 $dbh->do("START TRANSACTION");
 foreach (@lines) {
-    my @message_data = split (' ', $_);
-    my $log_str = join (' ', @message_data[2 .. $#message_data]);
-    my $created = join (' ', @message_data[0 .. 1]);
-    my ($id) = $_ =~ /id=(.*)$/;
-    my $int_id = $message_data[$message_internal_id];
-    
-    if ($message_data[$message_flag] eq '<=') {
-        if (defined $id) {
-            $counter++ if $dbh->do("INSERT INTO message VALUES (?, ?, ?, ?, ?)", undef, $created, $id, $int_id, $log_str, 1);
-        }
-        else {
-            print "No id for $log_str\n";
-        }
+  my @message_data = split(' ', $_);
+  my $log_str = join(' ', @message_data[ 2 .. $#message_data ]);
+  my $created = join(' ', @message_data[ 0 .. 1 ]);
+  my ($id) = $_ =~ /id=(.*)$/;
+  my $int_id = $message_data[$message_internal_id];
 
+  if ($message_data[$message_flag] eq '<=') {
+    if (defined $id) {
+      $counter++ if $dbh->do("INSERT INTO message VALUES (?, ?, ?, ?, ?)", undef, $created, $id, $int_id, $log_str, 1);
+    } else {
+      print "No id for $log_str\n";
     }
-    else {
-        my $recipient_email = $message_data[$message_email_address];
-        #print "$log_str\n";
-        $counter++ if $dbh->do("INSERT INTO log VALUES (?, ?, ?, ?)", undef, $created, $int_id, $log_str, $recipient_email);
-    }
+
+  } else {
+    my $recipient_email = $message_data[$message_email_address];
+
+    #print "$log_str\n";
+    $counter++ if $dbh->do("INSERT INTO log VALUES (?, ?, ?, ?)", undef, $created, $int_id, $log_str, $recipient_email);
+  }
 
 }
 
--- search.pl
+++ search.pl
@@ -3,35 +3,43 @@
 use strict;
 use warnings;
 use DBI;
- use Data::Dumper;
- 
-my $results = '';
+use Data::Dumper;
+
+
+my $html_data = '';
 # Connect to the database.
-my $dbh = DBI->connect("DBI:mysql:database=gazprom;host=localhost",
-                       "stan", "aqua32",
-                       {'RaiseError' => 1});
-                       
-my $search_query = 'ceftpqlvefv@mail.ru';
+my $dbh = DBI->connect("DBI:mysql:database=gazprom;host=localhost", "stan", "aqua32", { 'RaiseError' => 1 });
+
 my $limit = 100;
-#if (length ($ENV{'QUERY_STRING'}) > 0){
-   # $search_query = $ENV{'QUERY_STRING'};
-    my $events = $dbh->selectall_arrayref(
-    "(SELECT SQL_CALC_FOUND_ROWS message.created, message.int_id FROM message WHERE message.int_id IN (SELECT log.int_id FROM log WHERE log.address = ?)) UNION ALL (SELECT created, int_id FROM log WHERE address = ?) LIMIT ?",  { Slice => {} }, $search_query, $search_query, $limit
+my $counter = 0;
+$ENV{'QUERY_STRING'} = 'query=ceftpqlvefv@mail.ru';
+if (length($ENV{'QUERY_STRING'}) > 0) {
+  my ($search_query) = $ENV{'QUERY_STRING'} =~ /query=(.*)$/;
 
-);
+  my $events = $dbh->selectall_arrayref(
+"(SELECT SQL_CALC_FOUND_ROWS message.created, message.int_id FROM message WHERE message.int_id IN (SELECT log.int_id FROM log WHERE log.address = ?)) UNION ALL (SELECT created, int_id FROM log WHERE address = ?) LIMIT ?",
+    { Slice => {} }, $search_query, $search_query, $limit
+  );
 
-my $row_count = $dbh->selectrow_array("SELECT FOUND_ROWS()");
+  my $row_count = $dbh->selectrow_array("SELECT FOUND_ROWS()");
 
-foreach my $event ( @$events ) {
-    $results.= qq[Created: $event->{created} | Internal id:  $event->{int_id}<br>];
+  foreach my $event (@$events) {
+    $counter++;
+    $html_data .= qq[$counter) Created: $event->{created} | Internal id:  $event->{int_id}<br>];
+  }
+  if ($row_count > 100) {
+    $html_data .= qq[Всего результатов: $row_count, из них выведено $limit];
+  }
+
 }
-if ($row_count > 100) {
-    $results .= qq[Всего результатов: $row_count, из них выведено $limit];
+else {
+  $html_data .= qq[
+  <form action="" method="get">
+  <label for="site-search">Введите email:</label>
+  <input type="search" id="site-search" name="query">
+  <button>Search</button>
+  </form>];
 }
-#};
-
-
-
 my $html = qq[
 <!DOCTYPE html>
 <html lang="en">
@@ -45,9 +53,9 @@ my $html = qq[
 </head>
 
 <body>
-  
+
 <div class="container">
-  $results
+  $html_data
 </div>
 
 <script>
